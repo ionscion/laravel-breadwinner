757b6155334e09df264c5d441870efc6551d5295
bensboto <bensboto@gmail.com>
2023-07-11 16:56:52 -0600

    updated style for responsiveness

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-    <div class="flex flex-col flex-wrap items-center mt-10 mb-10 ">
-        <div class="flex flex-col bg-gray-200 bg-opacity-75 rounded-lg p-4 items-center">
-            <h2 class="text-2xl font-bold text-center underline">Classic French Loaf</h2>
-            <img class="mt-4 max-w-xl" src="{{ secure_asset('images/final-loaf.jpeg') }}" alt="Final Loaf of Bread">
-            <p class="mt-4 text-sm">Note: This recipe can be created either with a starter or with standard yeast. In this
-                example, we'll be using standard yeast.</p>
-        </div>
+<div class="flex flex-col flex-wrap items-center mt-10 mb-10">
+    <div class="flex flex-col bg-gray-200 bg-opacity-75 rounded-lg p-4 items-center">
+        <h2 class="text-2xl font-bold text-center underline text-gray-800">Classic French Loaf</h2>
+        <img class="mt-4 max-w-xs sm:max-w-xl" src="{{ asset('images/final-loaf.jpeg') }}" alt="Final Loaf of Bread">
+        <p class="mt-4 text-sm">Note: This recipe can be created either with a starter or with standard yeast. In this
+            example, we'll be using standard yeast.</p>
     </div>
+</div>
 
     <div class="flex flex-col items-center mt-10 mb-10">
         <div class="bg-gray-200 bg-opacity-75 rounded-lg p-4">
-            <h3 class="text-xl font-bold text-center">What You'll Need</h3>
+            <h3 class="text-xl font-bold text-center text-gray-800 underline">What You'll Need</h3>
             <ul class="mt-4 list-disc list-inside">
                 <li>*A quality baking/cooking scale is recommended*</li>
                 <li>Bread flour - 600 grams</li>
@@ -30,7 +30,7 @@
 
     <div class="flex flex-col items-center mt-10 mb-10">
         <div class="bg-gray-200 bg-opacity-75 rounded-lg p-4">
-            <h3 class="text-xl font-bold text-center">Instructions</h3>
+            <h3 class="text-xl font-bold text-center text-gray-800 underline">Instructions</h3>
             <ol class="mt-4 list-disc list-inside">
                 <li>Weigh out 600 grams of bread flour (I recommend King Arthur brand). Place weighed flour in Pyrex mixing
                     bowl. At this point it's a good idea to whisk the flour to ensure there are no clumps.</li>
@@ -43,7 +43,7 @@
                     keep
                     mixing and it will eventually come together.</li>
                 <li>After around 5 minutes of stirring, you should be left with a sticky glob of dough:</li>
-                <img class="m-4 max-w-lg mx-auto" src="{{ secure_asset('images/dough-proof.jpeg') }}" alt="Dough Glob">
+                <img class="m-4 max-w-xs mx-auto sm:max-w-xl" src="{{ asset('images/dough-proof.jpeg') }}" alt="Dough Glob">
                 <li>Cover your mixing bowl with a loose towel and let it sit on the counter for 2 hours.</li>
                 <li>At the 2 hour mark your dough should have risen quite a bit. Go ahead and punch it down, knead out any
                     significant air bubbles and form back into a ball in the bowl.</li>
@@ -62,7 +62,7 @@
                 <li>Place shaped ball into proofing basket, allow it settle while oven is preheating. By the time the oven
                     is
                     preheated, your proofing basket dough should hopefully have risen as follows:</li>
-                <img class="m-4 max-w-lg mx-auto" src="{{ secure_asset('images/proof-basket.jpeg') }}" alt="Dough Rise">
+                <img class="m-4 max-w-xs mx-auto sm:max-w-xl" src="{{ asset('images/proof-basket.jpeg') }}" alt="Dough Rise">
                 <li>Carefully remove dough from proofing basket and place gently in Cast Iron pot. Place lid on pot and put
                     it
                     on the center oven rack.</li>
@@ -77,7 +77,7 @@
 
     <div class="flex flex-col flex-wrap items-center mt-10 mb-10">
         <div class="bg-gray-200 bg-opacity-75 rounded-lg p-4">
-            <h4 class="text-xl font-bold text-center">Tips and Tricks</h4>
+            <h4 class="text-xl font-bold text-center text-gray-800 underline">Tips and Tricks</h4>
             <ul class="mt-4 list-disc list-inside">
                 <li>You can adjust the time for the final lid off bake up a bit if you like a much crispier crust.</li>
                 <li>Use a piece of parchment paper to line the bottom of the cast iron pot. This will help prevent the loaf
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>Ben's Breads</title>
     <style>
         .bg-custom {
-            background-image: url('{{ secure_asset('images/bg-purple.jpg') }}');
+            background-image: url('{{ asset('images/bg-purple.jpg') }}');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
@@ -19,20 +19,20 @@
 
 <body class="bg-custom">
     <nav class="bg-slate-300 bg-opacity-75 p-4 shadow-lg">
-        <h1 class="text-4xl font-bold text-teal-800/75">Ben's Breads!</h1>
+        <h1 class="text-4xl font-bold text-gray-800/75 underline">Ben's Breads!</h1>
     </nav>
     @yield('content')
 
     <div class="left-0 bottom-0 w-full bg-slate-300 bg-opacity-75 p-4 text-center shadow-lg">
         <div class="flex justify-center space-x-4">
             <a href="https://github.com/ionscion" target="_blank" rel="noreferrer">
-                <img src="{{secure_asset('images/github.svg')}}" alt="GitHub" class="w-6 h-6" />
+                <img src="{{asset('images/github.svg')}}" alt="GitHub" class="w-6 h-6" />
             </a>
             <a href="https://linkedin.com/in/ben-sboto-4b0040276" target="_blank" rel="noreferrer">
-                <img src="{{secure_asset('images/linkedin.svg')}}" alt="LinkedIn" class="w-6 h-6" />
+                <img src="{{asset('images/linkedin.svg')}}" alt="LinkedIn" class="w-6 h-6" />
             </a>
             <a href="https://stackoverflow.com/users/21884108/ionscion" target="_blank" rel="noreferrer">
-                <img src="{{secure_asset('images/stack-overflow.svg')}}" alt="Stack Overflow" class="w-6 h-6" />
+                <img src="{{asset('images/stack-overflow.svg')}}" alt="Stack Overflow" class="w-6 h-6" />
             </a>
         </div>
         <h6 class="text-gray-500">&copy; 2023 ionscion</h6>
